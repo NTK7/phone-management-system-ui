9db7d03dda74ecb2bf61ded084e08290ce379fe8
Nazhim <nazhimkalam@gmail.com>
2021-02-05 21:10:54 +0530

    commit

--- src/pages/AddInventory/AddInventory.js
+++ src/pages/AddInventory/AddInventory.js
@@ -80,216 +80,218 @@ function AddInventory() {
 	return (
 		<div className="addInventory">
 			<div className="addInventory__section">
-				<Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="tabs">
-					<Tab eventKey="home" title="Insert" className="tab">
-						<div className="tab__details">
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon1">Brand</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Brand"
-									aria-label="Enter Brand"
-									aria-describedby="basic-addon1"
-									value={insertBrand}
-									onChange={(e) => setInsertBrand(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon2">Model</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Model"
-									aria-label="Enter Model"
-									aria-describedby="basic-addon2"
-									value={insertModel}
-									onChange={(e) => setInsertModel(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon3">Vendor</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Vendor"
-									aria-label="Enter Vendor"
-									aria-describedby="basic-addon3"
-									value={insertVendor}
-									onChange={(e) => setInsertVendor(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon4">Quantity</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Quantity"
-									aria-label="Enter Quantity"
-									aria-describedby="basic-addon4"
-									value={insertQuantity}
-									onChange={(e) => setInsertQuantity(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon5">My Payment</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="My Payment"
-									aria-label="My Payment"
-									aria-describedby="basic-addon5"
-									value={insertMyPayment}
-									onChange={(e) => setInsertMyPayment(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon6">Pay Date</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Pay Date"
-									aria-label="Pay Date"
-									aria-describedby="basic-addon6"
-									value={insertPayDate}
-									onChange={(e) => setInsertPayDate(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon7">Pur Date</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Puy Date"
-									aria-label="Puy Date"
-									aria-describedby="basic-addon7"
-									value={insertPurDate}
-									onChange={(e) => setInsertPurDate(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon8">Total</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Total"
-									aria-label="Total"
-									aria-describedby="basic-addon8"
-									value={insertTotal}
-									onChange={(e) => setInsertTotal(e.target.value)}
-								/>
-							</InputGroup>
+				<div className="addInventory__sectionInner">
+					<Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="tabs">
+						<Tab eventKey="home" title="Insert" className="tab">
+							<div className="tab__details">
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon1">Brand</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Brand"
+										aria-label="Enter Brand"
+										aria-describedby="basic-addon1"
+										value={insertBrand}
+										onChange={(e) => setInsertBrand(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon2">Model</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Model"
+										aria-label="Enter Model"
+										aria-describedby="basic-addon2"
+										value={insertModel}
+										onChange={(e) => setInsertModel(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon3">Vendor</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Vendor"
+										aria-label="Enter Vendor"
+										aria-describedby="basic-addon3"
+										value={insertVendor}
+										onChange={(e) => setInsertVendor(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon4">Quantity</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Quantity"
+										aria-label="Enter Quantity"
+										aria-describedby="basic-addon4"
+										value={insertQuantity}
+										onChange={(e) => setInsertQuantity(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon5">My Payment</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter My Payment"
+										aria-label="Enter My Payment"
+										aria-describedby="basic-addon5"
+										value={insertMyPayment}
+										onChange={(e) => setInsertMyPayment(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon6">Pay Date</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Pay Date"
+										aria-label="Pay Date"
+										aria-describedby="basic-addon6"
+										value={insertPayDate}
+										onChange={(e) => setInsertPayDate(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon7">Pur Date</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Puy Date"
+										aria-label="Puy Date"
+										aria-describedby="basic-addon7"
+										value={insertPurDate}
+										onChange={(e) => setInsertPurDate(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon8">Total</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Total"
+										aria-label="Total"
+										aria-describedby="basic-addon8"
+										value={insertTotal}
+										onChange={(e) => setInsertTotal(e.target.value)}
+									/>
+								</InputGroup>
 
-							<div className="tab__buttonAction">
-								<Button onClick={insertRecord}>INSERT</Button>
+								<div className="tab__buttonAction">
+									<Button onClick={insertRecord}>INSERT</Button>
+								</div>
 							</div>
-						</div>
-					</Tab>
-					<Tab eventKey="profile" title="Update" className="tab">
-						<div className="tab__details">
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon9">Brand</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Brand"
-									aria-label="Enter Brand"
-									aria-describedby="basic-addon9"
-									value={updateBrand}
-									onChange={(e) => setUpdateBrand(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon10">Model</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Model"
-									aria-label="Enter Model"
-									aria-describedby="basic-addon10"
-									value={updateModel}
-									onChange={(e) => setUpdateModel(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon11">Vendor</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Vendor"
-									aria-label="Enter Vendor"
-									aria-describedby="basic-addon11"
-									value={updateVendor}
-									onChange={(e) => setUpdateVendor(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon12">Quantity</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Enter Quantity"
-									aria-label="Enter Quantity"
-									aria-describedby="basic-addon12"
-									value={updateQuantity}
-									onChange={(e) => setUpdateQuantity(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon13">My Payment</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="My Payment"
-									aria-label="My Payment"
-									aria-describedby="basic-addon13"
-									value={updateMyPayment}
-									onChange={(e) => setUpdateMyPayment(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon14">Pay Date</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Pay Date"
-									aria-label="Pay Date"
-									aria-describedby="basic-addon14"
-									value={updatePayDate}
-									onChange={(e) => setUpdatePayDate(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon15">Pur Date</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Puy Date"
-									aria-label="Puy Date"
-									aria-describedby="basic-addon15"
-									value={updatePurDate}
-									onChange={(e) => setUpdatePurDate(e.target.value)}
-								/>
-							</InputGroup>
-							<InputGroup className="mb-3">
-								<InputGroup.Prepend>
-									<InputGroup.Text id="basic-addon16">Total</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl
-									placeholder="Total"
-									aria-label="Total"
-									aria-describedby="basic-addon16"
-									value={updateTotal}
-									onChange={(e) => setUpdateTotal(e.target.value)}
-								/>
-							</InputGroup>
+						</Tab>
+						<Tab eventKey="profile" title="Update" className="tab">
+							<div className="tab__details">
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon9">Brand</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Brand"
+										aria-label="Enter Brand"
+										aria-describedby="basic-addon9"
+										value={updateBrand}
+										onChange={(e) => setUpdateBrand(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon10">Model</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Model"
+										aria-label="Enter Model"
+										aria-describedby="basic-addon10"
+										value={updateModel}
+										onChange={(e) => setUpdateModel(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon11">Vendor</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Vendor"
+										aria-label="Enter Vendor"
+										aria-describedby="basic-addon11"
+										value={updateVendor}
+										onChange={(e) => setUpdateVendor(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon12">Quantity</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter Quantity"
+										aria-label="Enter Quantity"
+										aria-describedby="basic-addon12"
+										value={updateQuantity}
+										onChange={(e) => setUpdateQuantity(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon13">My Payment</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Enter My Payment"
+										aria-label="Enter My Payment"
+										aria-describedby="basic-addon13"
+										value={updateMyPayment}
+										onChange={(e) => setUpdateMyPayment(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon14">Pay Date</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Pay Date"
+										aria-label="Pay Date"
+										aria-describedby="basic-addon14"
+										value={updatePayDate}
+										onChange={(e) => setUpdatePayDate(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon15">Pur Date</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Puy Date"
+										aria-label="Puy Date"
+										aria-describedby="basic-addon15"
+										value={updatePurDate}
+										onChange={(e) => setUpdatePurDate(e.target.value)}
+									/>
+								</InputGroup>
+								<InputGroup className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="basic-addon16">Total</InputGroup.Text>
+									</InputGroup.Prepend>
+									<FormControl
+										placeholder="Total"
+										aria-label="Total"
+										aria-describedby="basic-addon16"
+										value={updateTotal}
+										onChange={(e) => setUpdateTotal(e.target.value)}
+									/>
+								</InputGroup>
 
-							<div className="tab__buttonAction">
-								<Button onClick={updateRecord}>UPDATE</Button>
+								<div className="tab__buttonAction">
+									<Button onClick={updateRecord}>UPDATE</Button>
+								</div>
 							</div>
-						</div>
-					</Tab>
-				</Tabs>
+						</Tab>
+					</Tabs>
+				</div>
 			</div>
 		</div>
 	);

8b2d8c4ced333ee9a6799637c2decf49293df161
Nazhim <nazhimkalam@gmail.com>
2021-02-05 09:37:45 +0530

    commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,26 @@
-import './Home.css'
+import './Home.css';
 
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+	return (
+		<div className="home">
+			{/* nav bar for home */}
+			<div className="home__nav">
+				<h4>Phone Shop</h4>
+			</div>
+			{/* cards */}
+			<div className="home__cards container">
+				{/* inventory card */}
+				<div className="home__cardsInventory">
+					<p>INVENTORY</p>
+				</div>
+
+				{/* order card */}
+				<div className="home__cardsOrder">
+					<p>ORDER</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Home
+export default Home;

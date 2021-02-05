267c43bd889ea1f0065226093da53110478deedc
Nazhim <nazhimkalam@gmail.com>
2021-02-05 10:04:34 +0530

    completed the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
+	const [displayInventoryOptions, setDisplayInventoryOptions] = useState(false);
 	return (
 		<div className="home">
 			{/* nav bar for home */}
@@ -10,13 +13,26 @@ function Home() {
 			{/* cards */}
 			<div className="home__cards container">
 				{/* inventory card */}
-				<div className="home__cardsInventory">
-					<p>INVENTORY</p>
-				</div>
+				{displayInventoryOptions ? (
+					<div className="home__cardsInventory" onMouseLeave={() => setDisplayInventoryOptions(false)}>
+						<p>
+							<Link to="/add+inventory">ADD INVENTORY</Link>
+						</p>
+						<p>
+							<Link to="/view+inventory">VIEW INVENTOR</Link>
+						</p>
+					</div>
+				) : (
+					<div className="home__cardsInventory" onMouseEnter={() => setDisplayInventoryOptions(true)}>
+						<p>INVENTORY</p>
+					</div>
+				)}
 
 				{/* order card */}
 				<div className="home__cardsOrder">
-					<p>ORDER</p>
+					<p>
+						<Link to="/order">ORDER</Link>
+					</p>
 				</div>
 			</div>
 		</div>

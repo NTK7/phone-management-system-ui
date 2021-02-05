03cf38fe9dd7966660bcbd326916efd198a3e389
Nazhim <nazhimkalam@gmail.com>
2021-02-05 14:56:13 +0530

    commit

--- src/pages/AddInventory/AddInventory.js
+++ src/pages/AddInventory/AddInventory.js
@@ -1,11 +1,20 @@
-import './AddInventory.css'
+import { Tab } from 'bootstrap';
+import { Tabs } from 'react-bootstrap';
+import './AddInventory.css';
 
 function AddInventory() {
-    return (
-        <div className="addInventory">
-            Add Inventory
-        </div>
-    )
+	return (
+		<div className="addInventory">
+			<Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+				<Tab eventKey="home" title="Insert">
+					<div></div>
+				</Tab>
+				<Tab eventKey="profile" title="Update">
+					<div></div>
+				</Tab>
+			</Tabs>
+		</div>
+	);
 }
 
-export default AddInventory
+export default AddInventory;

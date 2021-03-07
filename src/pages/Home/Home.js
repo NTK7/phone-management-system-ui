be438ec76d692eaa71e268e511f1690c2b502b33
Nazhim <nazhimkalam@gmail.com>
2021-03-07 19:32:57 +0530

    added the days

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Card } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import './Home.css';
 
@@ -11,27 +12,85 @@ function Home() {
 			<div className="home__nav">
 				<h4>Phone Shop</h4>
 			</div>
+			<div className="home__cardsTop">
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 01</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 02</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 03</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 04</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 05</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 06</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+				<Card className="home__cardsTopCard">
+					<Card.Title>Day 07</Card.Title>
+					<Card.Text>Profit</Card.Text>
+					<Card.Text>100000</Card.Text>
+				</Card>
+			</div>
 			<div className="home__cards container">
-				{displayInventoryOptions ? (
-					<div className="home__cardsInventory" onMouseLeave={() => setDisplayInventoryOptions(false)}>
-						<p>
-							<Link to="/add+inventory">ADD INVENTORY</Link>
-						</p>
-						<p>
-							<Link to="/view+inventory">VIEW INVENTOR</Link>
-						</p>
+					{/* last payments section */}
+					<div className="lastPayments">
+						<Card>
+							<Card.Title>Last Payments</Card.Title>
+						</Card>
 					</div>
-				) : (
-					<div className="home__cardsInventory" onMouseEnter={() => setDisplayInventoryOptions(true)}>
-						<p>INVENTORY</p>
-					</div>
-				)}
 
-				<div className="home__cardsOrder" onClick = {() => {history.replace('/order')}}>
-					<p>
-						<Link to="/order">ORDER</Link>
-					</p>
-				</div>
+					{/* main menu container */}
+					<div className="mainMenuContainer">
+						{displayInventoryOptions ? (
+							<div className="home__cardsInventory" onMouseLeave={() => setDisplayInventoryOptions(false)}>
+								<p>
+									<Link to="/add+inventory">ADD INVENTORY</Link>
+								</p>
+								<p>
+									<Link to="/view+inventory">VIEW INVENTOR</Link>
+								</p>
+							</div>
+						) : (
+							<div className="home__cardsInventory" onMouseEnter={() => setDisplayInventoryOptions(true)}>
+								<p>INVENTORY</p>
+							</div>
+						)}
+
+						<div
+							className="home__cardsOrder"
+							onClick={() => {
+								history.replace('/order');
+							}}
+						>
+							<p>
+								<Link to="/order">ORDER</Link>
+							</p>
+						</div>
+					</div>
+					{/* items ends section */}
+					<div className="itemEnds">
+						<Card>
+							<Card.Title>Last Payments</Card.Title>
+						</Card>
+					</div>
 			</div>
 		</div>
 	);

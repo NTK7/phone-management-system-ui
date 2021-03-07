69956c04b623dcaa06975bc055194eaf63bd3199
Nazhim <nazhimkalam@gmail.com>
2021-03-07 14:19:46 +0530

    added redux for auth

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,16 +1,51 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from '../../features/userSlice';
 import './SignUp.css';
 
 function SignUp() {
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+	const dispatch = useDispatch();
+	const user = useSelector(selectUser);
+
+	// Sign In User
+	const signInUser = () => {
+		if (!email || !password) {
+			alert('Please fill all the fields!');
+		} else {
+			dispatch(
+				login({
+					email: 'some email',
+				})
+			);
+		}
+	};
+
+	// Sign Out User
+	const signOutUser = () => {
+		dispatch(logout());
+	};
+
 	return (
 		<div className="signUp">
 			<div className="signUp__container">
-				<h3>Sign in</h3>
-				<input type="text" placeholder="Email" />
-				<input type="text" placeholder="Password" />
+				{!user ? (
+					<>
+						<h3>Sign in</h3>
+						<input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+						<input type="text" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
 
-				<p>Forgot your password?</p>
+						<p>Forgot your password?</p>
 
-				<button>Sign In</button>
+						<button onClick={signInUser}>Sign In</button>
+					</>
+				) : (
+					<>
+						<h3>Sign out</h3>
+						<button onClick={signOutUser}>Sign Out</button>
+					</>
+				)}
 			</div>
 		</div>
 	);
